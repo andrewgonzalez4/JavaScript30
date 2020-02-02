48b7cf49b7814c3ff6daac2a24b76e356cfe149b
Andrew <andrew.gonzalez4@upr.edu>
2020-02-01 20:06:00 -0400

    Finished the first mini project of this free course.

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,17 @@
+window.addEventListener("keydown", function(event) {
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+  if (!audio) return; //If the key-code doesn't match one from the audio files simply return.
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
+  console.log(key);
+});
+
+function removeTransition(event) {
+  if (event.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
